0c6783ea01501488265b7958d793cdf11e46157c
EnzoKG <enzogiovanini@gmail.com>
2023-09-08 20:44:30 -0400

    Variáveis

---
+++ teste.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int idade; //Variáveis c++
+    idade = 21;
+    float pi = 3.14;
+    cout << idade << endl << pi;
+    return 0;
+}
